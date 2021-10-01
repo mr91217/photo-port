cd5aac7d2b4d5005fd0828ff729cc6a3b2b9a3af
ChunYu Chang <mr91217@gmail.com>
2021-09-30 22:48:33 -0400

    add .default for img

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PhotoList from '../PhotoList';
 import { capitalizeFirstLetter } from '../../utils/helpers';
+import photo from "../../assets/small/commercial/0.jpg";
+
 
 function Gallery(props) {
   const { currentCategory } = props;
@@ -9,6 +11,13 @@ function Gallery(props) {
       <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
       <p>{currentCategory.description}</p>
       <PhotoList category={currentCategory.name} />
+      {/* <div className="flex-row">
+          <img
+            src={photo}
+            alt="Commercial Example"
+            className="img-thumbnail mx-1"
+          />
+      </div> */}
     </section>
   );
 }
--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import Modal from '../Modal';
 
 const PhotoList = ({ category }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentPhoto, setCurrentPhoto] = useState();
 
   const [photos] = useState([
     {
@@ -102,31 +104,26 @@ const PhotoList = ({ category }) => {
   ]);
 
   const currentPhotos = photos.filter((photo) => photo.category === category);
-  const [currentPhoto, setCurrentPhoto] = useState();
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const toggleModal = (image, i) => {
-    // current photo
-    setCurrentPhoto({...image, index: i});
+    setCurrentPhoto({ ...image, index: i });
     setIsModalOpen(!isModalOpen);
-  }
+  };
 
   return (
     <div>
-      {isModalOpen && (<Modal currentPhoto={currentPhoto} onClose={toggleModal} />)}
+      {isModalOpen && <Modal onClose={toggleModal} currentPhoto={currentPhoto} />}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
-            src={require(`../../assets/small/${category}/${i}.jpg`)}
+            src={require(`../../assets/small/${category}/${i}.jpg`).default}
             alt={image.name}
             className="img-thumbnail mx-1"
             onClick={() => toggleModal(image, i)}
             key={image.name}
           />
         ))}
-        
       </div>
-      
     </div>
   );
 };
